ba55bcb5768baba52c0cbc42959e78fb270ebf41
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-05-19 17:44:48 -0400

    Improve cat and dog years tests

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         get_human_age(15, 15) == [1, 1]
         get_human_age(24, 24) == [2, 2]
     """
-    # TODO: Implement this function
+
     if cat_age < 15:
         cat_human_age = 0
 
--- app/test_main.py
+++ app/test_main.py
@@ -10,16 +10,22 @@ import pytest
         (28, 28, [3, 2]),
         (24, 24, [2, 2]),
         (23, 23, [1, 1]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (0, 0, [0, 0]),
+        (31, 31, [3, 3]),
+        (32, 32, [4, 3])
     ],
-    ids=[
+    ids={
         "should return zero human years when age is less than 15",
         "first 15 cat and dog years give 1 human year",
         "cat and dog should have different human age",
         "24 is the second human age",
         "23 is still the first human age",
         "100 years should return correct human years",
-    ]
+        "should return 0 human years when age is 0",
+        "should return 0 human years when age is 14",
+        "should return 2 human years when age is 27"
+    }
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert main.get_human_age(cat_age, dog_age) == expected
